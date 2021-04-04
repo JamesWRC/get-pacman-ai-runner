97b34e5c4347493a8dcf10e787e859b4137030b6
JamesWRC <38713144+JamesWRC@users.noreply.github.com>
2021-04-04 15:32:43 +1000

    Update testDIND.sh

--- testDIND.sh
+++ testDIND.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64 -t dindtest .
 
-docker run -v "/var/run/docker.sock:/var/run/docker.sock" dindtest
+docker run --privileged -v "/var/run/docker.sock:/var/run/docker.sock" dindtest
